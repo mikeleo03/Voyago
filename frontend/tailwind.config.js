4563c3792dea0a08cacf6131ca18cbf3967377f4
mikeleo03 <leonmichael463@gmail.com>
2024-09-14 14:52:31 +0700

    [Init] Setup TailwindCSS and Sparta-UI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,117 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        theme1: "#0A1020",
+        theme2: "#803811",
+        theme3: "#EC845E",
+        theme4: "#F5B768",
+        theme5: "#F5C1A0",
+        theme6: "#E5E6E0",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        green: {
+          1: '#2a3129',
+          2: '#28382d',
+          3: '#445c46'
+        },
+        orange: {
+          1: '#445c46',
+          2: '#f8641e',
+          3: '#f3900e'
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "scroll": {
+          to: { transform: "translate(calc(-50% - 0.5rem))" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "scroll": "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      width: {
+        '128': '32rem',
+      },
+      height: {
+        '128': '32rem',
+        '150': '39rem',
+        '1/12': '8.333333%',
+        '2/12': '16.666667%',
+        '3/12': '25%',
+        '4/12': '33.333333%',
+        '5/12': '41.666667%',
+        '6/12': '50%',
+        '7/12': '58.333333%',
+        '8/12': '66.666667%',
+        '9/12': '75%',
+        '10/12': '83.333333%',
+        '11/12': '91.666667%',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
+
