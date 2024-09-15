b9085e8bc28e419f374718752aff6bc4ff054876
mikeleo03 <leonmichael463@gmail.com>
2024-09-15 20:59:57 +0700

    [Refactor] Re-audit unimportant entity

--- backend/authentication/src/main/java/com/group4/authentication/data/model/User.java
+++ backend/authentication/src/main/java/com/group4/authentication/data/model/User.java
@@ -1,22 +1,13 @@
 package com.group4.authentication.data.model;
 
-import java.time.LocalDateTime;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,7 +22,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "User")
-@EntityListeners(AuditingEntityListener.class)
 public class User {
     
     @Id
@@ -58,17 +48,5 @@ public class User {
 
     @Column(name = "role", nullable = false)
     private String role = Role.CUSTOMER.toString();
-
-    @CreatedBy
-    private String createdBy;
-
-    @LastModifiedBy
-    private String updatedBy;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
 
