7c219ac2a57c21e7483352619e85e0a51f3beb4f
mikeleo03 <leonmichael463@gmail.com>
2024-09-16 09:09:07 +0700

    [Feat] Setup eureka client to handle dynamic port allocation

--- backend/gateway/src/main/java/com/group4/gateway/config/WebClientConfig.java
+++ backend/gateway/src/main/java/com/group4/gateway/config/WebClientConfig.java
@@ -1,6 +1,8 @@
 package com.group4.gateway.config;
 
 import com.group4.gateway.client.AuthClient;
+import com.netflix.discovery.EurekaClient;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,8 +16,8 @@ public class WebClientConfig {
     }
 
     @Bean
-    public AuthClient authClient(WebClient.Builder webClientBuilder) {
-        return new AuthClient(webClientBuilder);
+    public AuthClient authClient(EurekaClient eurekaClient, WebClient.Builder webClientBuilder) {
+        return new AuthClient(eurekaClient, webClientBuilder);
     }
 }
 
