5ec9a1e123cecf108a0efcb4fa5005f2fd3c286a
mikeleo03 <leonmichael463@gmail.com>
2024-09-16 09:08:40 +0700

    [Feat] Add some exceptions

---
+++ backend/gateway/src/main/java/com/group4/gateway/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.group4.gateway.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
