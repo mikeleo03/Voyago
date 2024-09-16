ca6db21c7a38081c715fad519467dbb0e7ffa54c
mikeleo03 <leonmichael463@gmail.com>
2024-09-16 08:14:13 +0700

    [Feat] Entity mapper

---
+++ backend/authentication/src/main/java/com/group4/authentication/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.group4.authentication.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.group4.authentication.data.model.User;
+import com.group4.authentication.dto.SignupRequest;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    // Mapping SignupRequest to User entity
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    User toUser(SignupRequest signupRequest);
+}
