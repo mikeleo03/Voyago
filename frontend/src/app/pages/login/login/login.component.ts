42e4119c37433678842e4793ca993aecbc1c641c
mikeleo03 <leonmichael463@gmail.com>
2024-09-19 19:08:43 +0700

    [Feat] Login component

--- frontend/src/app/pages/login/login/login.component.ts
+++ frontend/src/app/pages/login/login/login.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth/auth.service';
+import { LoginRequest } from '../../../models/user.model';
+import { ToastContainerDirective, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
+  standalone: true,  // Important! This marks the component as standalone.
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: [],
+  providers: [
+    AuthService
+  ], // Services can be provided directly in the component
+  imports: [FormsModule, CommonModule] // Import necessary modules directly into the component
 })
 export class LoginComponent {
+  // constants
+  mainbg: string = '../assets/img/login.png';
+  loginRequest: LoginRequest = { username: '', password: '' };
 
+  @ViewChild(ToastContainerDirective, { static: true })
+  toastContainer!: ToastContainerDirective;
+
+  constructor(private authService: AuthService, private router: Router, private toastrService: ToastrService) {}
+
+  ngOnInit() {
+    this.toastrService.overlayContainer = this.toastContainer;
+  }
+
+  login() {
+    this.authService.login(this.loginRequest).subscribe({
+      next: (response) => {
+        if (response.token) {
+          this.authService.setToken(response.token);
+          this.toastrService.success('Login successful!');
+          this.router.navigate(['/']);
+        } else {
+          this.toastrService.warning('Unexpected error occurred.');
+        }
+      },
+      error: (error) => {
+        if (error.status === 401) {
+          this.toastrService.error(error.error.error);
+        }
+      }
+    });
+  }  
 }
