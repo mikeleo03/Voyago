71de1ac080c86002a48679e223660f4ddeffcbc9
mikeleo03 <leonmichael463@gmail.com>
2024-09-21 10:03:30 +0700

    [Feat] Initiate 3 base component

---
+++ frontend/src/app/pages/change/change-username/change-username.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-username',
+  standalone: true,
+  imports: [],
+  templateUrl: './change-username.component.html',
+  styleUrl: './change-username.component.css'
+})
+export class ChangeUsernameComponent {
+
+}
---
+++ frontend/src/app/pages/change/change.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { ChangeUsernameComponent } from './change-username/change-username.component';
+import { ChangeConfirmedComponent } from './change-confirmed/change-confirmed.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+
+export const homeRoutes: Routes = [
+    { path: '/email', component: ChangeUsernameComponent },
+    { path: '/email/confirmed', component: ChangeConfirmedComponent },
+    { path: '/password/:id', component: ChangePasswordComponent }
+];
