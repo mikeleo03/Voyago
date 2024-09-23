52dd07e2309c0d279300405eb68f7b0a541bae49
mikeleo03 <leonmichael463@gmail.com>
2024-09-23 10:55:40 +0700

    [Feat] Update on spesification

--- backend/ticket/src/main/java/com/group4/ticket/data/repository/TicketDetailRepository.java
+++ backend/ticket/src/main/java/com/group4/ticket/data/repository/TicketDetailRepository.java
@@ -1,12 +1,10 @@
 package com.group4.ticket.data.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.group4.ticket.data.model.TicketDetail;
 
 @Repository
-public interface TicketDetailRepository extends JpaRepository<TicketDetail, UUID> {
+public interface TicketDetailRepository extends JpaRepository<TicketDetail, String> {
 }
--- backend/ticket/src/main/java/com/group4/ticket/data/repository/TicketRepository.java
+++ backend/ticket/src/main/java/com/group4/ticket/data/repository/TicketRepository.java
@@ -1,12 +1,10 @@
 package com.group4.ticket.data.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.group4.ticket.data.model.Ticket;
 
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+public interface TicketRepository extends JpaRepository<Ticket, String>, JpaSpecificationExecutor<Ticket> {
 }
+
