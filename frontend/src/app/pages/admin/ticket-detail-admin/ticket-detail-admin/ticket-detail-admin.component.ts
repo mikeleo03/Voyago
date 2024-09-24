2baf5a3e8e191610a2f7d5be80132a3a0e25889c
manuellaiv <13521051@std.stei.itb.ac.id>
2024-09-24 11:36:21 +0700

    [Feat] Ticket detail base component

---
+++ frontend/src/app/pages/admin/ticket-detail-admin/ticket-detail-admin/ticket-detail-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-detail-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket-detail-admin.component.html',
+  styleUrl: './ticket-detail-admin.component.css'
+})
+export class TicketDetailAdminComponent {
+
+}
