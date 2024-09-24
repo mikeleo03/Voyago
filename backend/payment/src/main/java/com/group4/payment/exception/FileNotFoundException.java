f485e5d5354ebbf2c662f0683aef51f7d3752319
mikeleo03 <leonmichael463@gmail.com>
2024-09-24 12:50:56 +0700

    [Feat] Add some exceptions

--- backend/payment/src/main/java/com/group4/payment/exception/FileNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.group4.payment.exception;
-
-public class FileNotFoundException extends RuntimeException{
-    public FileNotFoundException(String message) {
-        super(message);
-    }
-}
